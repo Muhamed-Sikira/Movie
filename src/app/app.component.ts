88e879770c03b96122740fb05abc3624ffe5422a
Muhamed-Sikira <117374063+Muhamed-Sikira@users.noreply.github.com>
2022-12-01 20:49:30 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'ms-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'PRVI-DIO';
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  get userName(): string {
+    if (this.authService.currentUser) {
+      return this.authService.currentUser.userName;
+    }
+    return '';
+  }
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logOut(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/welcome');
+  }
 }
