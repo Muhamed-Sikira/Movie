59771568632a6c1a8299f39ce1cdb007528a3b54
Muhamed-Sikira <117374063+Muhamed-Sikira@users.noreply.github.com>
2022-12-05 00:50:18 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
 import { MovieModule } from './movie/movie.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule, 
+    BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'home', component: HomeComponent},
-      {path: '', redirectTo: "home", pathMatch: "full"},
-      {path: '**', redirectTo: "home", pathMatch: "full"},
-    ]),
     MovieModule,
     UserModule,
-    MessageModule
+    MessageModule,
+    AppRoutingModule,
   ],
   bootstrap: [AppComponent],
 })
